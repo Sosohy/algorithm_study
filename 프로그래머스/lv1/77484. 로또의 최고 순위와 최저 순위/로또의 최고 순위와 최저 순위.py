bd08591bc3fdb708b152b85c07f9a176d774e4fd
HanSohye <34918049+Sosohy@users.noreply.github.com>
2023-03-28 22:40:27 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,5 +1,23 @@
 def solution(lottos, win_nums):
     answer = []
+    minNum = 0;
+    
+    for lotto in lottos:
+        if lotto in win_nums:
+            minNum += 1
+
+    maxNum = minNum + lottos.count(0)
+    
+    answer.insert(0, ((7-maxNum) if (7-maxNum < 6) else 6))
+    answer.insert(1, ((7-minNum) if (7-minNum < 6) else 6))
+    
+    return answer
+
+
+'''
+# 첫 번째 방법
+def solution(lottos, win_nums):
+    answer = []
     
     minNum = len(set(lottos) & set(win_nums))
     maxNum = minNum + lottos.count(0)
@@ -8,3 +26,4 @@ def solution(lottos, win_nums):
     answer.insert(1, ((7-minNum) if (7-minNum < 6) else 6))
     
     return answer
+'''
