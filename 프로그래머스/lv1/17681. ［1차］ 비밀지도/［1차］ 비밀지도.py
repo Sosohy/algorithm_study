cfead39db1fc636da51fb1f4989461867e238ad9
HanSohye <34918049+Sosohy@users.noreply.github.com>
2023-04-10 23:09:20 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -9,8 +9,23 @@ def getBinary(n, num):
         binary += '0'
         
     return binary[::-1]
-        
 
+def solution(n, arr1, arr2):
+    answer = []
+    
+    for i in range(n):
+        dec = ''
+        ar1 = getBinary(n, arr1[i])
+        ar2 = getBinary(n, arr2[i])
+        for j in range(n):
+            if(ar1[j] == '1' or ar2[j] == '1'):
+                dec += '#'
+            else:
+                dec += ' '
+        answer.append(dec)
+        
+    return answer
+'''
 def solution(n, arr1, arr2):
     trMap = [[' ' for j in range(n)] for i in range(n)]
     
@@ -26,3 +41,4 @@ def solution(n, arr1, arr2):
         answer.append(''.join(map(str, i)))
         
     return answer
+'''
