bd82eaa10094605237448734e4a258646d54bf19
HanSohye <34918049+Sosohy@users.noreply.github.com>
2023-04-19 15:24:04 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.14 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -0,0 +1,11 @@
+def solution(n, words):
+    answer = [0, 0]
+    turn = 1;
+    
+    for i in range(1, len(words)):
+        word = words[i]
+        
+        if (words[i-1][-1] != word[0]) or (word in words[:(i-1)]):
+            return [(i%n)+1, (i//n)+1]
+            
+    return answer
