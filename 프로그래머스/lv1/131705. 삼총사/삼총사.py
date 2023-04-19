a07fc1a0eccecd94916c5d17dc209b060fa69cb8
HanSohye <34918049+Sosohy@users.noreply.github.com>
2023-04-19 22:31:40 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -1,3 +1,17 @@
+from itertools import combinations
+
+def solution(number):
+    answer = 0
+    
+    combList = list(combinations(number, 3))
+    
+    for i in combList:
+        if sum(i) == 0:
+            answer += 1
+    
+    return answer
+
+'''
 def solution(number):
     answer = 0
     
@@ -9,3 +23,5 @@ def solution(number):
                     answer += 1
         
     return answer
+'''
+
