64fd9bfe0bd8adf161d0c1ffb4641c2229d52e47
HanSohye <34918049+Sosohy@users.noreply.github.com>
2023-05-30 21:29:38 +0900

    [level 2] Title: 프로세스, Time: 0.13 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.py
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -1,21 +1,17 @@
 def solution(priorities, location):
-    work = 0
     i = 0
     finish = 0
     
     while finish <= len(priorities):
         while True:
-            if (priorities[work] < priorities[i]):
-                work = i
+            if priorities[i] == max(priorities):
+                if i == location:
+                    return finish + 1
+                break
             else:
                 i = (i+1) % len(priorities)
-            
-            if priorities[work] == max(priorities): break
 
         finish += 1
-        priorities[work] = -1
-        
-        if(work == location):
-            return finish
+        priorities[i] = -1
 
     return len(priorities)
