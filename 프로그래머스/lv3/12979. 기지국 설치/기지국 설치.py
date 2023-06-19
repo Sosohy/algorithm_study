16b5fbef988b74f9b2d5e3e0c3c1f1f1f2b6083c
HanSohye <34918049+Sosohy@users.noreply.github.com>
2023-06-19 21:22:56 +0900

    [level 3] Title: 기지국 설치, Time: 2.25 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.py
@@ -2,16 +2,15 @@ import math
 
 def solution(n, stations, w):
     answer = 0
-    r = 2*w + 1
     
     start = 1
     for i in stations:
         apt = (i-w)-start
         if apt > 0:
-            answer += math.ceil(apt/r)
+            answer += math.ceil(apt/(2*w + 1))
         start = (i+w)+1
         
     if n >= start:
-        answer += math.ceil((n-start+ 1)/r)
+        answer += math.ceil((n-start+ 1)/(2*w + 1))
 
     return answer
