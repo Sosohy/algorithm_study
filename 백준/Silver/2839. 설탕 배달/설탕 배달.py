66be3cbe2e3323ec99c2fec6abff6a087a0637de
HanSohye <34918049+Sosohy@users.noreply.github.com>
2023-07-11 19:20:44 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,14 @@
+n = int(input())
+sugar = 0
+
+while(n>=0):
+    if(n%5 == 0):
+        sugar += n//5
+        print(sugar)
+        break
+    else:
+        n -= 3
+        sugar += 1
+
+if(n<0):
+    print(-1)
