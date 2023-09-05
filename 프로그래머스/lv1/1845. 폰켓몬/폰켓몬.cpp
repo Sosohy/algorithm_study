b9ac77f374c540b32e996a1cbe90317066e0d65d
HanSohye <34918049+Sosohy@users.noreply.github.com>
2023-09-05 20:44:34 +0900

    [level 1] Title: 폰켓몬, Time: 0.22 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.cpp
@@ -8,15 +8,7 @@ int solution(vector<int> nums)
     int answer = 0;
     set<int> dup(nums.begin(), nums.end());
     
-    // for(int i = 0; i<nums.size(); i++){
-    //     dup.insert(nums[i]);
-    // }
-    
-    if(dup.size() < nums.size()/2){
-        answer = dup.size();
-    }else{
-        answer = nums.size()/2;
-    }
+    answer = min(dup.size(), nums.size()/2);
     
     return answer;
 }
