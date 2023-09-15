27a86e297e906ef6073780a354be2c245211911a
HanSohye <34918049+Sosohy@users.noreply.github.com>
2023-09-15 15:33:06 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.15 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -2,21 +2,18 @@ def solution(survey, choices):
     answer = ''
     dicType = {"R":0, "T":0,"C":0,"F":0,"J":0,"M":0,"A":0,"N":0}
     
-    for i in range(len(survey)):
-        disagree = survey[i][0]
-        agree = survey[i][1]
+    for i, j in zip(survey, choices):
+        disagree = i[0]
+        agree = i[1]
         
-        j = choices[i]
-        if(j <= 4): #비동의일 경우
-            dicType[disagree] += 4 - j
-        elif(4 < j): #동의일 경우
-            dicType[agree] += j - 4
-    
-    values = list(dicType.values())
+        if(j<= 4): #비동의
+            dicType[disagree] += 4-j
+        else: #동의
+            dicType[agree] += j-4
+        
     keys = list(dicType.keys())
-    for i in range(0, len(dicType), 2):
-        m = max(values[i], values[i+1])
-        if(dicType[keys[i]] == m):
+    for i in range(0, len(keys), 2):
+        if(dicType[keys[i]] >= dicType[keys[i+1]]):
             answer += keys[i]
         else:
             answer += keys[i+1]
