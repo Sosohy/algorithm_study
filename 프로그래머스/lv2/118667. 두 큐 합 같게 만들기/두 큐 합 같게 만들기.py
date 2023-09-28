37a0250e75130066a34067b979cbcd669195a40a
HanSohye <34918049+Sosohy@users.noreply.github.com>
2023-09-28 10:45:17 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 69.64 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -6,13 +6,12 @@ def solution(queue1, queue2):
     q2 = deque(queue2)
     t1 = sum(queue1)
     t2 = sum(queue2)
-    
-    #홀수라 같을 수X 경우
-    if (t1 + t2)%2 != 0:
+
+    if (t1 + t2)%2 != 0: #홀수라 같을 수 없는 경우
         return -1
-    elif t1 == t2:
+    elif t1 == t2: #이미 같은 경우
         return answer
-    
+
     while answer <= (len(queue1)*2)*2:
         if(t1>t2):
             tmp = q1.popleft()
@@ -24,11 +23,9 @@ def solution(queue1, queue2):
             q1.append(tmp)
             t2 -= tmp
             t1 += tmp
-        
+
         answer += 1
-        #t1 = sum(q1)
-        #t2 = sum(q2)
         if(t1 == t2):
             return answer
-    
+
     return -1
