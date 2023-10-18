340ca4427c99f309cd5acb8cb4d8f5bca225572c
HanSohye <34918049+Sosohy@users.noreply.github.com>
2023-10-18 16:11:42 +0900

    [level unrated] Title: A로 B 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,7 +1,11 @@
 def solution(before, after):
     answer = 1
     
+    if(sorted(before) != sorted(after)):
+        return 0
+    '''
     for i in set(before):
         if(after.count(i) != before.count(i)):
             return 0
+    '''
     return answer
