e124e4ecb4a6171e454bc0ed7d0dd0d3baa9c756
HanSohye <34918049+Sosohy@users.noreply.github.com>
2023-10-19 11:14:09 +0900

    [level 3] Title: 등굣길, Time: 2.49 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -0,0 +1,12 @@
+def solution(m, n, puddles):
+    mapList = [[0 for i in range(m+1)] for j in range(n+1)]
+    mapList[1][1] = 1
+        
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if [j, i] in puddles:
+                mapList[i][j] = 0
+            else:
+                mapList[i][j] += (mapList[i-1][j]+mapList[i][j-1])%1000000007
+
+    return mapList[n][m]
