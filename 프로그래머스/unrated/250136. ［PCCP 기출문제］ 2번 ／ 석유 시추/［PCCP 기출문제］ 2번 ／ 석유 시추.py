5817053f2d6574cbe1d5707979415f4c2cac8087
HanSohye <34918049+Sosohy@users.noreply.github.com>
2023-11-27 20:27:18 +0900

    [level unrated] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 75.18 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -42,45 +42,6 @@ def solution(land):
         tmp = 0
         for i in v:
             tmp += dic[i]
-        
         answer = max(answer, tmp)
     
     return answer
-
-
-'''
-from collections import deque
-def solution(land):
-    answer = []
-    
-    def bfs(queue):
-        visit = [[0]*len(land[0]) for k in range(len(land))]
-        pos = [(0, -1), (-1, 0), (1, 0), (0, 1)]
-        oil = 0
-
-        while(queue):
-            x, y = queue.popleft()
-            if(not visit[x][y]):
-                visit[x][y] = 1
-                oil += 1
-
-            for k in range(4):
-                nx = x+pos[k][0]
-                ny = y+pos[k][1]
-
-                if 0<=nx<len(land) and 0<=ny<len(land[0]) and visit[nx][ny] == 0 and land[nx][ny]:
-                    queue.append((nx, ny))
-                    visit[nx][ny] = 1
-                    oil += 1
-                    
-        return oil
-            
-    for i in range(len(land[0])):
-        q = deque()
-        for j in range(len(land)):
-            if(land[j][i]):
-                q.append((j, i))
-        answer.append(bfs(q))
-    
-    return max(answer)
-'''
