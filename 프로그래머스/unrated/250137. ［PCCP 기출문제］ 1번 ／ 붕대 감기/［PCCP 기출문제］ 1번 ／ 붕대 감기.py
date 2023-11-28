725296a73f909cbf279ddd86b716594e634ed175
HanSohye <34918049+Sosohy@users.noreply.github.com>
2023-11-28 12:38:42 +0900

    [level unrated] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -2,6 +2,25 @@ def solution(bandage, health, attacks):
     answer = 0
     skillT, time = 0, 0
     char = health
+    
+    for at, damage in attacks:
+        skillT = at-time-1
+        
+        char += (skillT*bandage[1]) + (skillT//bandage[0])*bandage[2]
+        char = min(char, health)
+        time = at
+        char -= damage
+        
+        if char <= 0: return -1
+    
+    return char
+
+
+'''
+def solution(bandage, health, attacks):
+    answer = 0
+    skillT, time = 0, 0
+    char = health
     attack = attacks.pop(0)
     
     while(char > 0):
@@ -10,11 +29,12 @@ def solution(bandage, health, attacks):
         if attack[0] == time:
             skillT = 0
             char -= attack[1]
+            if(char <= 0):
+                    return -1
+                
             if attacks:
                 attack = attacks.pop(0)
             else:
-                if(char <= 0):
-                    return -1
                 return char
             continue
         
@@ -25,3 +45,4 @@ def solution(bandage, health, attacks):
         char = min(char+bandage[1], health)
     
     return -1
+'''
