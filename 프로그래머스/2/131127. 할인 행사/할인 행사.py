6a08563c05c8dd87b09cd03e956de75ce8fee011
HanSohye <34918049+Sosohy@users.noreply.github.com>
2024-07-12 17:14:15 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -3,22 +3,18 @@ def solution(want, number, discount):
     products = {want[i] : number[i] for i in range(len(want))}
     
     for i in range(10):  # 초기 10일 세팅
-        p = discount[i]
-        if(p in products):
-            products[p] -= 1
+        if(discount[i] in products):
+            products[discount[i]] -= 1
     
+    # 모든 원하는 제품이 원하는 수량만큼 있는지 확인
     def checkProducts(products, want):
-        for i in range(len(want)):
-            if(products[want[i]] > 0):
-                return False
-        return True
-        
-    for i in range(10, len(discount)):
-        
-        # 상품 개수 체크
-        if(checkProducts(products, want)):
+        return all(products[p] <= 0 for p in want)
+    
+    if(checkProducts(products, want)):
             answer += 1
         
+    for i in range(10, len(discount)):
+
         # 지난 첫째날 상품 빼기(10일 이전 날짜) 
         if(discount[i-10] in products):
             products[discount[i-10]] += 1
@@ -27,7 +23,7 @@ def solution(want, number, discount):
         if(discount[i] in products):
             products[discount[i]] -= 1
             
-    if(checkProducts(products, want)):
+        if(checkProducts(products, want)):
             answer += 1
 
     return answer
