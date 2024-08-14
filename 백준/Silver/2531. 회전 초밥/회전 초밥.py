02e3df6a0edc1ad4b68388828a87f7ee1a183fbe
HanSohye <34918049+Sosohy@users.noreply.github.com>
2024-08-14 11:17:53 +0900

    [Silver I] Title: 회전 초밥, Time: 3236 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -5,22 +5,17 @@ for i in range(n):
     sList.append(int(input()))
 
 eat = sList[:k]
-tmp = set(eat)
-tmp.add(c)
+types = set(eat)
+types.add(c)
 
-answer = len(tmp)
-
-s, e = 0, k
+answer = len(types)
 
 for i in range(n):
     eat.pop(0)
-    eat.append(sList[e])
+    eat.append(sList[(k+i)%n])
 
-    tmp2 = set(eat)
-    tmp2.add(c)
-    answer = max(answer, len(tmp2))
-    
-    e = (e+1)%n
+    types = set(eat)
+    types.add(c)
+    answer = max(answer, len(types))
 
 print(answer)
-
