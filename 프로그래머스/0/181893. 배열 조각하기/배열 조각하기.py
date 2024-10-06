51d41acf08cbb86204a600c185e77292b93f96e0
HanSohye <34918049+Sosohy@users.noreply.github.com>
2024-10-07 01:08:25 +0900

    [level 0] Title: 배열 조각하기, Time: 1.19 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
@@ -1,7 +1,9 @@
 def solution(arr, query):
+    
     for i in range(len(query)):
-        if i % 2 == 0 :
+        if(i%2 == 0):
             arr = arr[:query[i]+1]
-        else :
+        else:
             arr = arr[query[i]:]
+        
     return arr
