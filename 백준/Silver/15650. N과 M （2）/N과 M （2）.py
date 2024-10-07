c853807fa7d4cb72adddd48e23dcb6844d831466
HanSohye <34918049+Sosohy@users.noreply.github.com>
2024-10-07 11:19:42 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+
+selected = [0 for i in range(m)]
+
+def per(nList, idx):
+    if(idx >= m):
+        print(" ".join(list(map(str, nList))))
+    else:
+        s = 1 if idx == 0 else nList[idx-1]+1
+        for i in range(s, n+1):
+            nList[idx] = i
+            per(nList, idx+1)
+            nList[idx] = 0
+
+per(selected, 0)
