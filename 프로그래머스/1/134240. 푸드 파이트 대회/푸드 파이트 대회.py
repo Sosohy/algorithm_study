253572e51b4484ea77e287e0b69b0b20ed49afdb
HanSohye <34918049+Sosohy@users.noreply.github.com>
2024-10-19 10:19:47 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,9 +1,10 @@
 def solution(food):
-    answer = ""
+    answer = ''
     
-    for i in range(len(food)):
-        answer += (str(i) * (food[i]//2))
+    for i in range(1, len(food)):
+        
+        if(food[i] > 1):
+            answer += str(i)*(int(food[i])//2)
     
-    answer += ("0" + answer[::-1])  
-    
+    answer = answer + "0" + answer[::-1]
     return answer
