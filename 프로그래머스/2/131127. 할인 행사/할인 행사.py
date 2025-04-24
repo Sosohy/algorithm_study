d14f8e91a885aedacd314149458953f43f28d417
HanSohye <34918049+Sosohy@users.noreply.github.com>
2025-04-24 21:59:13 +0900

    [level 2] Title: 할인 행사, Time: 60.48 ms, Memory: 14.9 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,29 +1,34 @@
 def solution(want, number, discount):
     answer = 0
-    products = {want[i] : number[i] for i in range(len(want))}
+    dic = {}
     
-    for i in range(10):  # 초기 10일 세팅
-        if(discount[i] in products):
-            products[discount[i]] -= 1
+    for i in range(len(want)):
+        dic[want[i]] = number[i]
     
-    # 모든 원하는 제품이 원하는 수량만큼 있는지 확인
-    def checkProducts(products, want):
-        return all(products[p] <= 0 for p in want)
+    for i in range(10):
+        if(discount[i] in dic):
+            dic[discount[i]] -= 1
     
-    if(checkProducts(products, want)):
-            answer += 1
-        
-    for i in range(10, len(discount)):
-
-        # 지난 첫째날 상품 빼기(10일 이전 날짜) 
-        if(discount[i-10] in products):
-            products[discount[i-10]] += 1
-        
-        # 추가된 마지막날 상품 추가하기
-        if(discount[i] in products):
-            products[discount[i]] -= 1
+    def isTrue():
+        for key in list(dic.keys()):
+            if(dic[key] > 0):
+                return False
+                break
             
-        if(checkProducts(products, want)):
-            answer += 1
+        return True
+            
+    if(isTrue()):
+        answer += 1
 
+    for i in range(10, len(discount)):
+        
+        if(discount[i-10] in dic):
+            dic[discount[i-10]] += 1
+        
+        if(discount[i] in dic):
+            dic[discount[i]] -= 1
+        
+        if(isTrue()):
+            answer += 1
+    
     return answer
