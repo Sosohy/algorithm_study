2793218950f582eb4d00bb7ac3e33906d481fd7c
HanSohye <34918049+Sosohy@users.noreply.github.com>
2025-04-27 14:27:17 +0900

    [level 2] Title: H-Index, Time: 6.28 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,9 +1,14 @@
 def solution(citations):
-    answer = len(citations)
+    answer = 0
+    n = len(citations)
     citations.sort(reverse=True)
     
-    for i in range(len(citations)):
-        if i+1 > citations[i]:
+    for i in range(n, -1, -1):
+        tmp = 0
+        for j in range(n):
+            if(citations[j] >= i):
+                tmp += 1
+        if(tmp >= i and n-tmp <= i):
             answer = i
             break
         
